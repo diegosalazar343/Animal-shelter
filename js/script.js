037167aa2dd4fe4f93c694cf4ff53a1a6c83e1ed
diegosalazar343 <salazardiego343@gmail.com>
2020-08-11 13:57:55 -0700

    add background color, highlight button of a certian section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-  $("button#dark").click(function() {
+  $("button#night").click(function() {
     $("body").removeClass();
     $("body").addClass("dark-background");
   });
 
-  $("button#light").click(function() {
+  $("button#day").click(function() {
     $("body").removeClass();
     $("body").addClass("light-background");
+  });
+
+  $("button#highlight").click(function() {
+    $("p1").removeClass();
+    $("p1").addClass("highlight");
+    //why won't the highlight go away?
   });
 });
